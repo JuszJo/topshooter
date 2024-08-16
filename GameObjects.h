ca1b5e8c0b41e5038e914d428118cdc88ce42ad6
JuszJo <joshubaniwokoma@gmail.com>
2024-08-16 13:15:38 +0100

    umbrella header

---
+++ GameObjects.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
+
+#include "Entity.h"
+#include "Square.h"
