b6e4e5ae453a851ebf6bae770b51f68b253a5cf3
JuszJo <joshubaniwokoma@gmail.com>
2024-08-18 18:07:17 +0100

    use with create entity helper

--- Bullet.h
+++ Bullet.h
@@ -8,25 +8,12 @@ Bullet createBullet(GameObjectType owner) {
 
     bullet.owner = owner;
 
-    bullet.entity.active = true;
+    Entity entity = createEntity(GameObjectType::BULLET, 10.0f, 10.0f, glm::vec3(0.0f, 0.0f, 0.0f));
+
+    bullet.entity = entity;
 
     bullet.entity.glAttributes.stride = 3;
     bullet.entity.glAttributes.offset = 0;
-
-    bullet.entity.gameObjectType = BULLET;
-
-    bullet.entity.size = glm::vec2(10.0f, 10.0f);
-    bullet.entity.position = glm::vec3(0.0f, 0.0f, 0.0f);
-    bullet.entity.velocity = glm::vec3(0.0f, 0.0f, 0.0f);
-    bullet.entity.color = glm::vec3(0.0f, 0.0f, 0.0f);
-    bullet.entity.model = glm::mat4(1.0f);
-
-    bullet.entity.vertices = {
-        glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(bullet.entity.size.x, 0.0f, 0.0f),
-        glm::vec3(0.0f, bullet.entity.size.y, 0.0f),
-        glm::vec3(bullet.entity.size.x, bullet.entity.size.y, 0.0f),
-    };
 
     genGLAttributes(&bullet.entity);
 
