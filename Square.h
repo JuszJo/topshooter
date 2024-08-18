773aec0f8f3346e354c928b5550dfcdfce95d0d8
JuszJo <joshubaniwokoma@gmail.com>
2024-08-18 18:08:34 +0100

    refactor to use create ent helper

--- Square.h
+++ Square.h
@@ -1,25 +1,8 @@
 Entity createSquare() {
-    Entity entity;
-
-    entity.active = true;
+    Entity entity = createEntity(GameObjectType::PLAYER, 30.0f, 30.0f, glm::vec3(0.0f, 0.0f, 0.0f));
 
     entity.glAttributes.stride = 3;
     entity.glAttributes.offset = 0;
-
-    entity.gameObjectType = PLAYER;
-
-    entity.size = glm::vec2(30.0f, 30.0f);
-    entity.position = glm::vec3(0.0f, 0.0f, 0.0f);
-    entity.velocity = glm::vec3(0.0f, 0.0f, 0.0f);
-    entity.color = glm::vec3(0.0f, 0.0f, 0.0f);
-    entity.model = glm::mat4(1.0f);
-
-    entity.vertices = {
-        glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(entity.size.x, 0.0f, 0.0f),
-        glm::vec3(0.0f, entity.size.y, 0.0f),
-        glm::vec3(entity.size.x, entity.size.y, 0.0f),
-    };
 
     genGLAttributes(&entity);
 
